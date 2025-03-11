097919f1400800ceb6187a14db797ca3e7b4d1f9
César <cesarmarino00@gmail.com>
2025-03-11 18:04:55 -0300

    add commons

---
+++ test/ProvaPub.Test.IntegrationTest/Commons/FixtureBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProvaPub.Infrastructure.Data.Contexts;
+
+namespace ProvaPub.Test.IntegrationTest.Commons
+{
+    public abstract class FixtureBase
+    {
+        public CancellationToken CancellationToken { get; } = default;
+
+        public ProvaPubContext MakeContext() => new(
+            new DbContextOptionsBuilder<ProvaPubContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options);
+    }
+}
