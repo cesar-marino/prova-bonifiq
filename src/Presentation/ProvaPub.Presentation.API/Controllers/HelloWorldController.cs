21f6614cce292439438d3e9ecc43ee421e9139dd
César <cesarmarino00@gmail.com>
2025-03-11 18:18:39 -0300

    add hello world controller

---
+++ src/Presentation/ProvaPub.Presentation.API/Controllers/HelloWorldController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProvaPub.Presentation.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Hello() => Ok("Hello World!");
+    }
+}
