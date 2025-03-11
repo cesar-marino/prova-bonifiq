586b9374903b6af10b8078d24868dafd0149ac23
César <cesarmarino00@gmail.com>
2025-03-11 16:57:24 -0300

    ensure generate random number on create entity

--- src/Domain/ProvaPub.Domain/Entities/RandonNumberEntity.cs
+++ src/Domain/ProvaPub.Domain/Entities/RandonNumberEntity.cs
@@ -5,5 +5,16 @@ namespace ProvaPub.Domain.Entities
     public class RandonNumberEntity : EntityBase
     {
         public int Number { get; }
+
+        public RandonNumberEntity()
+        {
+            Number = GenerateRandonNumber();
+        }
+
+        public int GenerateRandonNumber()
+        {
+            var randon = new Random(Id.GetHashCode());
+            return randon.Next();
+        }
     }
 }
---
+++ test/ProvaPub.Test.UnitTest/Domain/Entities/RandonNumberEntityTest.cs
@@ -0,0 +1,16 @@
+﻿using ProvaPub.Domain.Entities;
+
+namespace ProvaPub.Test.UnitTest.Domain.Entities
+{
+    public class RandonNumberEntityTest
+    {
+        [Fact(DisplayName = nameof(ShouldGeneraterandonNumberOnCreateEntity))]
+        [Trait("Unit/Entities", "RandonNumber")]
+        public void ShouldGeneraterandonNumberOnCreateEntity()
+        {
+            var randonNumber = new RandonNumberEntity();
+
+            Assert.NotEqual(0, randonNumber.Number);
+        }
+    }
+}
