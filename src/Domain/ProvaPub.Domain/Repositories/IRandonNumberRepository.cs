8ae24791c5018a7b34956296ecc5ccbcdce9650f
César <cesarmarino00@gmail.com>
2025-03-11 10:58:07 -0300

    restarting the project

--- src/Domain/ProvaPub.Domain/Repositories/IRandonNumberRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using ProvaPub.Domain.Entities;
-using ProvaPub.Domain.SeedWork;
-
-namespace ProvaPub.Domain.Repositories
-{
-    public interface IRandonNumberRepository : IRepository<RandonNumberEntity>
-    {
-    }
-}
--- src/Domain/ProvaPub.Domain/SeedWork/IRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProvaPub.Domain.SeedWork
-{
-    public interface IRepository<TEntity> where TEntity : EntityBase
-    {
-        Task<TEntity> GetAsync(Guid id, CancellationToken cancellationToken = default);
-        Task InsertAsync(TEntity entity, CancellationToken cancellationToken = default);
-        Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
-    }
-}
