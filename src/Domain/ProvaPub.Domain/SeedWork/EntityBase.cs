3e13aa16dd7399beeb1079e1e392c343a50abe86
César <cesarmarino00@gmail.com>
2025-03-11 11:02:44 -0300

    add seed work

---
+++ src/Domain/ProvaPub.Domain/SeedWork/EntityBase.cs
@@ -0,0 +1,7 @@
+﻿namespace ProvaPub.Domain.SeedWork
+{
+    public abstract class EntityBase(Guid? id = null)
+    {
+        public Guid Id { get; } = id ?? Guid.NewGuid();
+    }
+}
---
+++ src/Domain/ProvaPub.Domain/SeedWork/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ProvaPub.Domain.SeedWork
+{
+    public interface IRepository<TEntity> where TEntity : EntityBase
+    {
+        Task<IReadOnlyList<TEntity>> FindAll(CancellationToken cancellationToken = default);
+        Task<TEntity> FindAsync(Guid id, CancellationToken cancellationToken = default);
+        Task InsertAsync(TEntity entity, CancellationToken cancellationToken = default);
+        Task RemoveAsync(Guid id, CancellationToken cancellationToken = default);
+        Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+    }
+}
