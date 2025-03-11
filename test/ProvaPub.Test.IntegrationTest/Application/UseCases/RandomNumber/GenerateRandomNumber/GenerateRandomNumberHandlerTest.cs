3fb3bd4427662272027019b432668b7e1122e840
César <cesarmarino00@gmail.com>
2025-03-11 18:07:38 -0300

    add generate random number integration tests

---
+++ test/ProvaPub.Test.IntegrationTest/Application/UseCases/RandomNumber/GenerateRandomNumber/GenerateRandomNumberHandlerTest.cs
@@ -0,0 +1,6 @@
+﻿namespace ProvaPub.Test.IntegrationTest.Application.UseCases.RandomNumber.GenerateRandomNumber
+{
+    public class GenerateRandomNumberHandlerTest : IClassFixture<GenerateRandomNumberHandlerTestFixture>
+    {
+    }
+}
---
+++ test/ProvaPub.Test.IntegrationTest/Application/UseCases/RandomNumber/GenerateRandomNumber/GenerateRandomNumberHandlerTestFixture.cs
@@ -0,0 +1,8 @@
+﻿using ProvaPub.Test.IntegrationTest.Commons;
+
+namespace ProvaPub.Test.IntegrationTest.Application.UseCases.RandomNumber.GenerateRandomNumber
+{
+    public class GenerateRandomNumberHandlerTestFixture : FixtureBase
+    {
+    }
+}
