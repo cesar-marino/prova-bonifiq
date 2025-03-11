ce6dd49b9239f3766a097967bf792e54ba086ac1
César <cesarmarino00@gmail.com>
2025-03-11 18:31:32 -0300

    add product entity

---
+++ src/Domain/ProvaPub.Domain/Entities/ProductEntity.cs
@@ -0,0 +1,19 @@
+﻿using ProvaPub.Domain.SeedWork;
+
+namespace ProvaPub.Domain.Entities
+{
+    public class ProductEntity : EntityBase
+    {
+        public string Name { get; }
+
+        public ProductEntity(string name)
+        {
+            Name = name;
+        }
+
+        public ProductEntity(Guid productId, string name) : base(productId)
+        {
+            Name = name;
+        }
+    }
+}
