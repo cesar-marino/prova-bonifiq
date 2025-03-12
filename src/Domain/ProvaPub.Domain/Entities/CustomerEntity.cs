bc2e5ab5d971924ac5975cfa4d4f57dab082f83f
César <cesarmarino00@gmail.com>
2025-03-12 18:32:27 -0300

    add customer entity

---
+++ src/Domain/ProvaPub.Domain/Entities/CustomerEntity.cs
@@ -0,0 +1,24 @@
+﻿using ProvaPub.Domain.SeedWork;
+
+namespace ProvaPub.Domain.Entities
+{
+    public class CustomerEntity : EntityBase
+    {
+        public string Name { get; }
+
+        private readonly List<Guid> _orders;
+        public IReadOnlyList<Guid> Orders => _orders;
+
+        public CustomerEntity(string name)
+        {
+            _orders = [];
+            Name = name;
+        }
+
+        public CustomerEntity(Guid customerId, string name) : base(customerId)
+        {
+            _orders = [];
+            Name = name;
+        }
+    }
+}
