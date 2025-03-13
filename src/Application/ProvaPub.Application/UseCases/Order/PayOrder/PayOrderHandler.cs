1cf3c2d1ccf1069e7cd914e125ac090506f27d13
César <cesarmarino00@gmail.com>
2025-03-13 13:29:57 -0300

    ensure rethrow same exception that CommitAsync throws

--- src/Application/ProvaPub.Application/UseCases/Order/PayOrder/PayOrderHandler.cs
+++ src/Application/ProvaPub.Application/UseCases/Order/PayOrder/PayOrderHandler.cs
@@ -2,12 +2,14 @@
 using ProvaPub.Application.UseCases.Order.Commons;
 using ProvaPub.Domain.Entities;
 using ProvaPub.Domain.Repositories;
+using ProvaPub.Domain.SeedWork;
 
 namespace ProvaPub.Application.UseCases.Order.PayOrder
 {
     public class PayOrderHandler(
         IPaymentFacade paymentFacade,
-        IOrderRepository orderRepository) : IPayOrderHandler
+        IOrderRepository orderRepository,
+        IUnitOfWork unitOfWork) : IPayOrderHandler
     {
         public async Task<OrderResponse> Handle(PayOrderRequest request, CancellationToken cancellationToken)
         {
@@ -15,7 +17,7 @@ namespace ProvaPub.Application.UseCases.Order.PayOrder
 
             var order = new OrderEntity(value: request.Amount);
             await orderRepository.InsertAsync(order, cancellationToken);
-
+            await unitOfWork.CommitAsync(cancellationToken);
 
             throw new NotImplementedException();
         }
--- test/ProvaPub.Test.UnitTest/Application/UseCases/Order/PayOrder/PayOrderHandlerTest.cs
+++ test/ProvaPub.Test.UnitTest/Application/UseCases/Order/PayOrder/PayOrderHandlerTest.cs
@@ -4,6 +4,7 @@ using ProvaPub.Application.UseCases.Order.PayOrder;
 using ProvaPub.Domain.Entities;
 using ProvaPub.Domain.Exceptions;
 using ProvaPub.Domain.Repositories;
+using ProvaPub.Domain.SeedWork;
 
 namespace ProvaPub.Test.UnitTest.Application.UseCases.Order.PayOrder
 {
@@ -13,16 +14,19 @@ namespace ProvaPub.Test.UnitTest.Application.UseCases.Order.PayOrder
         private readonly PayOrderHandler _sut;
         private readonly Mock<IPaymentFacade> _paymentFacadeMock;
         private readonly Mock<IOrderRepository> _orderRepositoryMock;
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
 
         public PayOrderHandlerTest(PayOrderHandlerTestFixture fixture)
         {
             _fixture = fixture;
             _paymentFacadeMock = new();
             _orderRepositoryMock = new();
+            _unitOfWorkMock = new();
 
             _sut = new(
                 paymentFacade: _paymentFacadeMock.Object,
-                orderRepository: _orderRepositoryMock.Object);
+                orderRepository: _orderRepositoryMock.Object,
+                unitOfWork: _unitOfWorkMock.Object);
         }
 
         [Fact(DisplayName = nameof(ShouldRethrowSameExceptionThatProcessThrows))]
@@ -51,6 +55,22 @@ namespace ProvaPub.Test.UnitTest.Application.UseCases.Order.PayOrder
                 .Setup(x => x.InsertAsync(
                     It.IsAny<OrderEntity>(),
                     It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new UnexpectedException());
+
+            var request = _fixture.MakePayOrderRequest();
+            var act = () => _sut.Handle(request, _fixture.CancellationToken);
+
+            var exception = await Assert.ThrowsAsync<UnexpectedException>(act);
+            Assert.Equal("unexpected", exception.Code);
+            Assert.Equal("An unexpected error occurred", exception.Message);
+        }
+
+        [Fact(DisplayName = nameof(ShouldRethrowSameExceptionThatCommitAsyncThrows))]
+        [Trait("Unit/UseCases", "Order - PayOrder")]
+        public async Task ShouldRethrowSameExceptionThatCommitAsyncThrows()
+        {
+            _unitOfWorkMock
+                .Setup(x => x.CommitAsync(It.IsAny<CancellationToken>()))
                 .ThrowsAsync(new UnexpectedException());
 
             var request = _fixture.MakePayOrderRequest();
