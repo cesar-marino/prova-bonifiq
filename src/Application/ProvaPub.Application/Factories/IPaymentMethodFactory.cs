4a5ad4ba753b4f87b47a7184003c0c2645217fbf
César <cesarmarino00@gmail.com>
2025-03-13 12:00:45 -0300

    add payment method factory

---
+++ src/Application/ProvaPub.Application/Factories/IPaymentMethodFactory.cs
@@ -0,0 +1,9 @@
+﻿using ProvaPub.Domain.Strategies;
+
+namespace ProvaPub.Application.Factories
+{
+    public interface IPaymentMethodFactory
+    {
+        IPaymentStrategy Create(string method);
+    }
+}
---
+++ src/Application/ProvaPub.Application/Factories/PaymentMethodFactory.cs
@@ -0,0 +1,22 @@
+﻿using ProvaPub.Domain.Exceptions;
+using ProvaPub.Domain.Strategies;
+using System.Reflection;
+
+namespace ProvaPub.Application.Factories
+{
+    public class PaymentMethodFactory : IPaymentMethodFactory
+    {
+        public IPaymentStrategy Create(string method)
+        {
+            var paymentMethods = Assembly.GetExecutingAssembly()
+                .GetTypes()
+                .Where(t => typeof(IPaymentStrategy).IsAssignableFrom(t) && !t.IsInterface)
+                .ToDictionary(t => t.Name.ToLower().Replace("payment", ""), t => t);
+
+            if (paymentMethods.TryGetValue(method.ToLower(), out var paymentType))
+                return (IPaymentStrategy)Activator.CreateInstance(paymentType)!;
+
+            throw new NotFoundException("Payment method");
+        }
+    }
+}
