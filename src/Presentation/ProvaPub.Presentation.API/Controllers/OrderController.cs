8231abaed51d2c5f1861a2e8f31b92f5b5b5997e
César <cesarmarino00@gmail.com>
2025-03-13 15:30:10 -0300

    add order controller

---
+++ src/Presentation/ProvaPub.Presentation.API/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProvaPub.Application.UseCases.Order.PayOrder;
+
+namespace ProvaPub.Presentation.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OrderController(IMediator mediator) : ControllerBase
+    {
+        [HttpPost("pay")]
+        public async Task<IActionResult> Pay(
+            [FromBody] PayOrderRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            var response = await mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+    }
+}
