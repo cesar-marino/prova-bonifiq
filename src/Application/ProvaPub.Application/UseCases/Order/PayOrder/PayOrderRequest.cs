ad30714236740acf89503fbe36d48a8b3e6915fd
César <cesarmarino00@gmail.com>
2025-03-13 11:41:32 -0300

    add attributes to PayOrderRequest

--- src/Application/ProvaPub.Application/UseCases/Order/PayOrder/PayOrderRequest.cs
+++ src/Application/ProvaPub.Application/UseCases/Order/PayOrder/PayOrderRequest.cs
@@ -3,7 +3,13 @@ using ProvaPub.Application.UseCases.Order.Commons;
 
 namespace ProvaPub.Application.UseCases.Order.PayOrder
 {
-    public class PayOrderRequest : IRequest<OrderResponse>
+    public class PayOrderRequest(
+        Guid customerId,
+        string paymentMethod,
+        decimal amount) : IRequest<OrderResponse>
     {
+        public Guid CustomerId { get; } = customerId;
+        public string PaymentMethod { get; } = paymentMethod;
+        public decimal Amount { get; } = amount;
     }
 }
