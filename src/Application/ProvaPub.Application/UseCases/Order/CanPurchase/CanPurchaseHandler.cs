92d058bc38e382a95a602fb9c1dabcb856aebb74
César <cesarmarino00@gmail.com>
2025-03-15 15:15:14 -0300

    add can purchase use case

--- src/Application/ProvaPub.Application/UseCases/Order/CanPurchase/CanPurchaseHandler.cs
+++ src/Application/ProvaPub.Application/UseCases/Order/CanPurchase/CanPurchaseHandler.cs
@@ -5,6 +5,12 @@ namespace ProvaPub.Application.UseCases.Order.CanPurchase
     {
         public Task<bool> Handle(CanPurchaseRequest request, CancellationToken cancellationToken)
         {
+            //O valor da compra deve ser maior que zero
+            //o cliente deve estar registrar para realizar a compra
+            //o cliente pode comprar apenas uma vez por mês
+            //A primeira compra deve ter valor máximo de 100 reais
+            //O cliente pode comprar apenas em horário comercial
+
             throw new NotImplementedException();
         }
     }
