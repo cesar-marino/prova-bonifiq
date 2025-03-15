45c57009cbd09a4063271a45b6c93ad33d91c3d3
César <cesarmarino00@gmail.com>
2025-03-15 16:11:17 -0300

    add purchase amount specification

---
+++ src/Domain/ProvaPub.Domain/Specifications/PurchaseAmountSpecification.cs
@@ -0,0 +1,12 @@
+﻿using ProvaPub.Domain.Entities;
+
+namespace ProvaPub.Domain.Specifications
+{
+    public class PurchaseAmountSpecification : ISpecification<OrderEntity>
+    {
+        public bool IsSatisfiedBy(OrderEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
