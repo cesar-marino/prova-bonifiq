6e59b0d45772a79d358143bcfbd02f7b2ecf1305
César <cesarmarino00@gmail.com>
2025-03-15 17:10:34 -0300

    ensure return true if the purchase amount  is less than or equal to one handred reais

--- test/ProvaPub.Test.UnitTest/Domain/Specifications/FirstPurchaseSpecificationTest.cs
+++ test/ProvaPub.Test.UnitTest/Domain/Specifications/FirstPurchaseSpecificationTest.cs
@@ -19,5 +19,21 @@ namespace ProvaPub.Test.UnitTest.Domain.Specifications
 
             Assert.False(result);
         }
+
+        [Theory(DisplayName = nameof(ShouldReturnTrueIfThePurchaseAmountIsLessThanOrEqualToOneHundredReais))]
+        [Trait("Unit/Specifications", "FirstPurchase")]
+        [InlineData(100)]
+        [InlineData(99)]
+        [InlineData(1)]
+        public void ShouldReturnTrueIfThePurchaseAmountIsLessThanOrEqualToOneHundredReais(int amount)
+        {
+            var orders = new List<OrderEntity>();
+            var order = new OrderEntity(customerId: Guid.NewGuid(), amount: amount);
+            var spec = new FirstPurchaseSpecification(orders);
+
+            var result = spec.IsSatisfiedBy(order);
+
+            Assert.True(result);
+        }
     }
 }
