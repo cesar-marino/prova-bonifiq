62d0ca06d66bc3fa88c39878170e68b12db31813
César <cesarmarino00@gmail.com>
2025-03-15 17:00:27 -0300

    add first purchase specification

---
+++ src/Domain/ProvaPub.Domain/Specifications/FirstPurchaseSpecification.cs
@@ -0,0 +1,12 @@
+﻿using ProvaPub.Domain.Entities;
+
+namespace ProvaPub.Domain.Specifications
+{
+    public class FirstPurchaseSpecification(IReadOnlyList<OrderEntity> orders) : ISpecification<OrderEntity>
+    {
+        public bool IsSatisfiedBy(OrderEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
