125013c19a2180b20598667e60229e407fb1820d
César <cesarmarino00@gmail.com>
2025-03-15 15:29:38 -0300

    ensure throw EntityValidationException if the amount is less than or equal to zero

--- src/Domain/ProvaPub.Domain/Specifications/CustomerMonthlyPurchaseLimitSpecification.cs
+++ src/Domain/ProvaPub.Domain/Specifications/CustomerMonthlyPurchaseLimitSpecification.cs
@@ -2,7 +2,7 @@
 
 namespace ProvaPub.Domain.Specifications
 {
-    public class CustomerMonthlyPurchaseLimitSpecification : ISpecification<List<OrderEntity'>>
+    public class CustomerMonthlyPurchaseLimitSpecification : ISpecification<List<OrderEntity>>
     {
         public string ErrorMessage => throw new NotImplementedException();
 
