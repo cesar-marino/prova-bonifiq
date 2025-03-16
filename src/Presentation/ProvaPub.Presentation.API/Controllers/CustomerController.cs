6a290a3ef3ae1b6271d0995c4d55cd3d283814d4
César <cesarmarino00@gmail.com>
2025-03-16 13:23:59 -0300

    add customer controller

---
+++ src/Presentation/ProvaPub.Presentation.API/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProvaPub.Application.UseCases.Customer.CanPurchase;
+using ProvaPub.Application.UseCases.Customer.ListCustomers;
+
+namespace ProvaPub.Presentation.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CustomerController(IMediator mediator) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> List(
+            [FromQuery] int page,
+            [FromQuery] int? perPage = null,
+            CancellationToken cancellationToken = default)
+        {
+            var request = new ListCustomersRequest(page: page);
+            if (perPage != null)
+                request.PerPage = perPage.Value;
+
+            var response = await mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CanPurchase(
+            [FromBody] CanPurchaseRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            var response = await mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+    }
+}
