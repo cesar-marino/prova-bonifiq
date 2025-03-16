50d2b5808e40bbfd4045b015f75885d477d7a683
César <cesarmarino00@gmail.com>
2025-03-16 13:05:49 -0300

    ensure return false if it is not the first purchase of the month

--- test/ProvaPub.Test.IntegrationTest/Application/UseCases/Customer/CanPurchase/CanPurchaseHandlerTest.cs
+++ test/ProvaPub.Test.IntegrationTest/Application/UseCases/Customer/CanPurchase/CanPurchaseHandlerTest.cs
@@ -88,5 +88,35 @@ namespace ProvaPub.Test.IntegrationTest.Application.UseCases.Customer.CanPurchas
 
             Assert.False(response);
         }
+
+        [Theory(DisplayName = nameof(ShouldReturnFalseIfItIsNotTheFirstPurchaseOfTheMonth))]
+        [Trait("Integration/UseCases", "Customer - CanPurchase")]
+        [InlineData(101)]
+        [InlineData(1000)]
+        public async Task ShouldReturnFalseIfItIsNotTheFirstPurchaseOfTheMonth(decimal amount)
+        {
+            var context = _fixture.MakeContext();
+            var customer = _fixture.MakeCustomerModel();
+            var order = _fixture.MakeOrderModel();
+
+            var trackingInfo = await context.Customers.AddAsync(customer);
+
+            await context.SaveChangesAsync();
+            trackingInfo.State = EntityState.Detached;
+
+            var customerRepository = new CustomerRepository(context);
+            var orderRepository = new OrderRepository(context);
+            var canPurchaseSpecificationFactory = new CanPurchaseSpecificationFactory();
+
+            var sut = new CanPurchaseHandler(
+                customerRepository: customerRepository,
+                orderRepository: orderRepository,
+                canPurcahaseSpecificationFactory: canPurchaseSpecificationFactory);
+
+            var request = _fixture.MakeCanPurchaseRequest(customerId: customer.CustomerId, amount: amount);
+            var response = await sut.Handle(request, _fixture.CancellationToken);
+
+            Assert.False(response);
+        }
     }
 }
--- test/ProvaPub.Test.IntegrationTest/Commons/FixtureBase.cs
+++ test/ProvaPub.Test.IntegrationTest/Commons/FixtureBase.cs
@@ -25,5 +25,15 @@ namespace ProvaPub.Test.IntegrationTest.Commons
             string? name = null) => new(
                 customerId: customerId ?? Faker.Random.Guid(),
                 name: name ?? Faker.Commerce.ProductName());
+
+        public OrderModel MakeOrderModel(
+            Guid? orderId = null,
+            Guid? customerId = null,
+            decimal? amount = null,
+            DateTime? orderDate = null) => new(
+                orderId: orderId ?? Faker.Random.Guid(),
+                amount: amount ?? Faker.Finance.Amount(),
+                orderDate: orderDate ?? DateTime.Now,
+                customerId: customerId ?? Faker.Random.Guid());
     }
 }
