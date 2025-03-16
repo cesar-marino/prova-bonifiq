d8761f4136cd3bb5ac40f04b48fc880698d23f93
César <cesarmarino00@gmail.com>
2025-03-16 12:34:57 -0300

    .

--- src/Application/ProvaPub.Application/UseCases/Order/CanPurchase/CanPurchaseHandler.cs
+++ src/Application/ProvaPub.Application/UseCases/Order/CanPurchase/CanPurchaseHandler.cs
@@ -21,21 +21,6 @@ namespace ProvaPub.Application.UseCases.Order.CanPurchase
                 amount: request.Amount);
 
             return specComposte.IsSatisfiedBy(order);
-
-            //Recuperar o customer (se não existir lança NotFoundException - Atende a regra 2)
-            //Cria uma Order (se o valor de amount for menor que 1 lança Exception - Deve ser atendida no construtor da classe order)
-
-            //Recupera a lista de compras realizada pelo cliente
-
-
-            //2. O cliente deve estar registrado para realizar a compra
-
-            //1. O valor da compra deve ser maior que zero
-            //3. O cliente pode comprar apenas uma vez por mês
-            //4. A primeira compra deve ter valor máximo de 100 reais
-            //5. O cliente pode comprar apenas em horário comercial
-
-            //throw new NotImplementedException();
         }
     }
 }
